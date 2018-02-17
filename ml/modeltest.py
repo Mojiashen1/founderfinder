326c9aff4285e34a4988b761b16e2b8291aa5ed5
Magnus Johnson <magntron@mit.edu>
2018-02-16 23:24:49 -0800

    Removed some unnecessary comments from modeltest.py

--- ml/modeltest.py
+++ ml/modeltest.py
@@ -2,8 +2,6 @@ import torch
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
 N, D_in, H, D_out = 1000, 50, 50, 1
 
 x = Variable(torch.randn(N, D_in))
@@ -26,30 +24,17 @@ model = torch.nn.Sequential(
 
 loss_fn = torch.nn.MSELoss(size_average=False)
 
-learning_rate = 1e-5
+learning_rate = 1e-4
 for t in range(500):
-  # Forward pass: compute predicted y by passing x to the model. Module objects
-  # override the __call__ operator so you can call them like functions. When
-  # doing so you pass a Variable of input data to the Module and it produces
-  # a Variable of output data.
   y_pred = model(x)
 
-  # Compute and print loss. We pass Variables containing the predicted and true
-  # values of y, and the loss function returns a Variable containing the loss.
   loss = loss_fn(y_pred, y)
   print(t, loss.data[0])
   
-  # Zero the gradients before running the backward pass.
   model.zero_grad()
-
-  # Backward pass: compute gradient of the loss with respect to all the learnable
-  # parameters of the model. Internally, the parameters of each Module are stored
-  # in Variables with requires_grad=True, so this call will compute gradients for
-  # all learnable parameters in the model.
   loss.backward()
 
-  # Update the weights using gradient descent. Each parameter is a Variable, so
-  # we can access its data and gradients like we did before.
+  # update the weights using gradient descent
   for param in model.parameters():
     param.data -= learning_rate * param.grad.data
 
